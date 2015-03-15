cfb8872ac70fc10e124c1aad9a688cfb16eb65a1
Ricky H <rickylqhan@gmail.com>
2015-03-15 00:10:36 -0500

    Update rice.py

---
+++ rice.py
@@ -0,0 +1,12 @@
+from rice.search import search_software, search_rice
+import argparse
+
+parser = argparse.ArgumentParser(description='RiceDB cmdline cli')
+parser.add_argument('program', help='program to rice')
+parser.add_argument('rice', help='rice package')
+
+args = parser.parse_args()
+if args.rice:
+  print(search_rice(args.program, args.rice))
+else:
+  print(search_software(args.program))
