d9d38bf077592e0c04864558dab01aa09eda4c0a
Ezra <kizunanohikari@gmail.com>
2015-03-17 14:59:47 -0500

    Renamed setup to swapin to avoid naming conflicts and altered function to support planned hashing methods

--- src/rice/swapin.py
+++ src/rice/swapin.py
@@ -13,7 +13,7 @@ def getHash(file_name, blocksize=2**20):
             sha.update(buf)
     return sha.digest()
 
-def setup(program_name, rice_name, file_dict):
+def swapin(program_name, rice_name, file_dict):
     """
     Takes a program name, rice name, and a dictionary of vanilla files/locations
     and will move files into locations specified by sysinfo.json and the file_dict
@@ -21,23 +21,14 @@ def setup(program_name, rice_name, file_dict):
     """
     os.chdir('~/.riceDB/' + program_name + '/' + rice_name + '/')
     json_data = open("sysinfo.json")
-    sha_checksums = open("checksums.sha")
     data = json.load(json_data)
-    checksums = json.load(sha_checksums)
     json_data.close()
-    sha_checksums.close()
     for file_name in data.keys():
-        if getHash(file_name) is checksums[file_name]:
-            download.checkpath(data[file_name])
-            os.rename('./' + file_name, data[file_name] + file_name)
-        else:
-            return (False, "WARNING, file checksums did not match expected values. Please ensure your connection is secure and that you have an up-to-date index file")
+        download.checkpath(data[file_name])
+        os.rename('./' + file_name, data[file_name] + file_name)
     for file_name in file_dict.keys():
-        if getHash(file_name) is checksums[file_name]:
-            download.checkpath(file_dict[file_name])
-            os.rename('./' + file_name, file_dict[file_name] + file_name)
-        else:
-            return (False, "WARNING, file checksums did not match expected values. Please ensure your connection is secure and that you have an up-to-date index file")
+        download.checkpath(file_dict[file_name])
+        os.rename('./' + file_name, file_dict[file_name] + file_name)
     os.chdir('~/.riceDB/' + program_name + '/')
     with open('.active', 'w') as fout:
         fout.write(rice_name)
