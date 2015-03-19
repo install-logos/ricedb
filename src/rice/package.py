fb8161f5e2c2ea309dd787610df0b4e3a3ae2928
Ezra <kizunanohikari@gmail.com>
2015-03-19 16:10:38 -0500

    Added basic switchin functionality

--- src/rice/package.py
+++ src/rice/package.py
@@ -29,32 +29,33 @@ class Package(object):
 
     if ("URL" in self.data):
       self.url = self.data["URL"]
-    else:
+    elif not old:
       raise error.CorruptionError("Could not determine URL of package.")
 
     if ("Images" in self.data):
       self.images = self.data["Images"]
-    
+
     if ("Program" in self.data):
         self.program = self.data["Program"]
     else:
         raise error.CorruptionError("Could not determine the program of the package")
-    
+
     if ("Files" in self.data):
         self.files = self.data["Files"]
     else:
         raise error.CorruptionError("Could not determine the files of the package")
-  
+
     if ("Path" in self.data):
         self.conf_root = self.data["Path"]
     else:
       raise error.CorruptionError("Could not determine the installation location of the package")
-  def download(self):
-    # Get the path of the download
+
     self.prog_path = util.RBDIR + self.program
     self.path = util.RDBDIR + self.program + '/'+ self.name
     self.install_file = self.path + INSTALL
 
+  def download(self):
+    # Get the path of the download
     if not (os.path.exists(util.RDBDIR) and os.path.isdir(util.RDBDIR)):
       os.mkdir(util.RDBDIR)
     if not (os.path.exists(prog_path)):
@@ -91,51 +92,34 @@ class Package(object):
     if "Files" in self.install_data:
         file_locs = self.install_data['Files']
     else raise error.CorruptionError("Could not read the files in the JSON")
-    switch_out(self.prog_name) 
+    switch_out() 
+    switch_in()
 
-  def switch_out(prog_name):
+  def switch_out():
     os.chdir(self.prog_path)
     if(os.path.exists('./.active') and os.path.isfile('./.active')):
         #if rice is a riceDB rice, read sysinfo.json to move non-vanilla files
         active_rice = open('./.active').readline().rstrip()
         if not os.path.exists('./' + active_rice):
-            raise error.CorruptionError("the .active file referenced a nonexistant package")
+            raise error.CorruptionError("The .active file referenced a nonexistant package")
         os.chdir('./' + active_rice)
         for k in self.Files.keys():
             os.rename(os.expanduser(self.conf_root + self.Files[k] + k), './' + k)
-        return (active_rice, True, "")
+        return active_rice
     else:
         # TODO - Get switchout working for a local rice
-        #else ask user for non-vanilla files
-        user_files = {}
-        another = 'y'
-        rice_name = ""
-        while another == 'y':
-            print("Please enter the path of your non-vanilla files:\n (type n to continue)")
-            user_inp = input()
-            if os.path.isfile(user_inp):
-                t = os.path.split(user_inp)
-                user_files[t[1]]=(t[0] + '/')
-                print("Do you have another non-vanilla file beside the ones you already typed in ? (y/n)")
-                another = input().lower()
-            elif user_inp is 'n':
-                break 
-            elif user_inp is '':
-                break 
-            else:
-                print("Filepath " + user_inp + " is not valid, please retry.")
-        print("Please choose a name for your personnal rice:")
-        rice_name = input()
-        while os.path.exists('~/.riceDB/' + prog_name + '/' + rice_name):
-            print("A rice with this name already exists, please choose another name:")
-            rice_name = input()
-        os.mkdir(rice_name)
-        # os.chdir('./' + rice_name + '/')
-        swfiles(user_files,rice_name)
-        swfiles(dict_file,'./')
-        if len(user_files) > 0:
-            serialize(user_file,'sysinfo.json')
-        else:
-            with open('sysinfo.json','w') as fout:
-                fout.write("{\n}")
-        return (rice_name,True,"")
+
+  def switch_in():
+    os.chdir(self.path)
+    if not (os.path.exists(self.conf_root)):
+        os.mkdir(self.conf_root)
+    for k in self.Files.keys():
+        if not (os.path.exists('./' + k)):
+            os.chdir(self.prog_path)
+            switch_in(open('./.active').readline().rstrip())
+            raise error.CorruptionError("Nonexistant files referenced in install.json")    
+        os.rename('./' + k, os.expanduser(self.conf_root) + self.Files[k] + k)
+    os.chdir(self.prog_path)
+    with open('./.active','w') as fout:
+        fout.write(self.name)
+
