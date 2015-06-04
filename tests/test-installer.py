7874fc8f67045f88faac613614337a31b14f1133
Vector919 <manbitesdog326@gmail.com>
2015-06-03 20:43:19 -0400

    forcing install

--- tests/test-installer.py
+++ tests/test-installer.py
@@ -29,7 +29,7 @@ class testInstaller(unittest.TestCase):
             self.test_url
         )
         i3_test.download()
-        i3_test.install()
+        i3_test.install(force=True)
         self.assertTrue(os.path.exists(os.path.expanduser("~/.rdb/i3/test1/file1.conf")))
         self.assertTrue(os.path.exists(os.path.expanduser("~/.rdb/i3/test1/install.json")))
         self.assertTrue(os.path.exists(os.path.expanduser("~/.i3/file2.conf")))
